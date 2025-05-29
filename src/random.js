413549d26bebc4fc9291da2ac3add34ca1cb924d
Luca Maltagliati <lmalta95@gmail.com>
2025-05-29 17:03:03 +0000

    cleanup

--- src/random.js
+++ src/random.js
@@ -1,27 +1,41 @@
-const GOOGLE_API_KEY='AIzaSyASHd9h4n4AVXDFgUGT9K8Ie4zaHzepIBA'
-      
-      function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-      }
-      fetch(
-        "https://sheets.googleapis.com/v4/spreadsheets/1iMoIDMyHRUSPY9WDSc_JDwQy2e4CrHQUX_AWp0oO6a4/values/A:B",
-        {
-          headers: new Headers({
-            "accept":"application/json",
-            "x-goog-api-key": GOOGLE_API_KEY
-          })
-        }).then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
+const GOOGLE_API_KEY = 'AIzaSyASHd9h4n4AVXDFgUGT9K8Ie4zaHzepIBA'
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+async function fetchSheet() {
+  const response = await fetch(
+    "https://sheets.googleapis.com/v4/spreadsheets/1iMoIDMyHRUSPY9WDSc_JDwQy2e4CrHQUX_AWp0oO6a4/values/A:A",
+    {
+      headers: new Headers({
+        "accept": "application/json",
+        "x-goog-api-key": GOOGLE_API_KEY
       })
-      .then(data => {
-        console.log('Fetched JSON:', data);
-        const randIdx = getRandomInt(data.values.length)
-        const row = data.values[randIdx]
-        //document.getElementById("phrase_number").innerHTML = row[0]
-        document.getElementById("phrase_text").innerHTML = row[1]
-      }).catch(err => {
-          document.getElementById("phrase_text").innerHTML = err
-      })
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  return await response.json();
+}
+
+async function init() {
+  try {
+    const data = await fetchSheet();
+    console.log('Fetched JSON:', data);
+
+    const { values } = data;
+
+    const randIdx = getRandomInt(values.length)
+    const row = values[randIdx]
+
+    document.getElementById("phrase_text").innerHTML = row[0]
+  } catch (err) {
+    document.getElementById("phrase_text").innerHTML = err;
+  }
+}
+
+init()
